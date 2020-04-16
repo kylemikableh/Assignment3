087f74033e00bc208c541347322f3da1d8825c5e
Patrick Siegler <ptrains95@gmail.com>
2020-04-16 17:03:03 -0400

    Set up objects (Problems 6-8)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    //Part6 and 7
     Client Jo = new Client("Jo",32);
     Client Riley = new Client("Riley",473);
     Client Peyton = new Client ("Peyton",555);
@@ -9,7 +10,20 @@ public class Main {
     SalesAssociate Sven = new SalesAssociate("Sven",Charlie,7);
     SalesAssociate Alex = new SalesAssociate("Alex",Sam,20);
 
+    ArrayList<Employee> samEmployees = new ArrayList<Employee>();
+    samEmployees.add(Alex);
+    ArrayList<Employee> CharlieEmployees = new ArrayList<Employee>();
+    CharlieEmployees.add(Sven);
+
+    SalesManager Sam = new SalesManager("Sam",samEmployees,null,3);
+    SalesManager Charlie = new SalesManager("Charlie",CharlieEmployees,null,4);
+
+    Sam.addClient(Jo);
+    Sam.addClient(Riley);
+    Charlie.addClient(Avery);
+    Sven.addClient(Peyton);
+    Alex.addClient(Taylor);
+    Alex.addClient(Remi);
 
 
-    //SalesManager Sam = new SalesManager("Sam",)
 }
