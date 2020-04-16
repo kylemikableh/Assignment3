24ebcf741785ea95d06a8b6dc6b240459ebb81e7
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2020-04-16 16:48:29 -0400

    Implemented both Client and Employee

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,44 @@
-public class Client {
+public class Client implements Comparable<Client> {
+    private int clientID;
+    private String clientName;
+    private Employee employee;
+
+    public Client (int clientID, String clientName, Employee employee) {
+        this.clientID = clientID;
+        this.clientName = clientName;
+        this.employee = employee;
+    }
+
+    protected Employee getEmployee() {
+        return employee;
+    }
+
+    protected void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    protected int getClientID() {
+        return clientID;
+    }
+
+    protected String getClientName() {
+        return clientName;
+    }
+
+    protected void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    protected void setClientName(String client) {
+        this.clientName = client;
+    }
+
+    protected double totalSales() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Client o) {
+        return clientID;
+    }
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,18 @@
-public class Employee {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class Employee {
+
+    private ArrayList<Client> list = new ArrayList<Client>();
+
+    public boolean addClient(Client client) {
+        boolean didAdd = list.add(client);
+        Collections.sort(list);
+        return didAdd;
+    }
+
+    private ArrayList<Client> getClients()
+    {
+        return list;
+    }
 }
